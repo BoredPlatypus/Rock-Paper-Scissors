ac63a674b8dec5440b461b7d1ac722ebaf4ad0e6
BoredPlatypus <167495200+BoredPlatypus@users.noreply.github.com>
2025-02-14 11:48:25 +0530

    Wrote the logic to end the game once either score reaches 5 and declare the final winner

--- script.js
+++ script.js
@@ -32,77 +32,99 @@ function getHumanChoice(playerChoice){
 
 
 function playGame(){
+
+        let humanChoice = 0;
+        let computerChoice = 0;
+        
         const choiceRock = document.querySelector("#rock");
         choiceRock.addEventListener("click" , () =>{
-            console.log(getHumanChoice("rock"));
-
+            humanChoice = getHumanChoice("rock");
+            computerChoice = getComputerChoice();
+            playRound(humanChoice,computerChoice);
+            gameOver();
         });
         const choicePaper = document.querySelector("#paper");
         choicePaper.addEventListener("click", ()=>{
-            console.log(getHumanChoice("paper"));
+            humanChoice = getHumanChoice("paper");
+            computerChoice = getComputerChoice();
+            playRound(humanChoice,computerChoice);
+            gameOver();
         });
         const choiceScissors = document.querySelector("#scissors");
         choiceScissors.addEventListener("click", ()=>{
-            console.log(getHumanChoice("scissors"));
+            humanChoice = getHumanChoice("scissors");
+            computerChoice = getComputerChoice();
+            playRound(humanChoice,computerChoice);
+            gameOver();
         });
 
 
         let humanScore = 0; // variable to keep track of Player Score.
         let computerScore = 0; // variable to keep track of Computer Score.
+        let scoreDiv = document.querySelector(".score");
+        let currentResultDiv = document.querySelector(".winorlose");
+        let finalResult = document.querySelector(".declarewinner");
+        scoreDiv.style.fontSize = "25px";
+        scoreDiv.style.color = "brown";
+        currentResultDiv.style.fontSize = "25px";
+        currentResultDiv.style.color = "brown";
+        finalResult.style.fontSize = "25px";
+        finalResult.style.color = "brown";
 
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
+        
 
         function playRound(humanChoice,computerChoice){
+            if(humanScore === 5 || computerScore === 5){
+                return;
+            }
             if(humanChoice === computerChoice){
-                alert("Draw!");
+                currentResultDiv.textContent = "Draw!";
+                scoreDiv.textContent = "Your Score: " + humanScore +" Computer Score: " + computerScore;
             }
             else if(humanChoice === "rock" && computerChoice ==="paper"){
                 computerScore++;
-                alert("You Lose! Paper beats Rock.");
-                alert("Your Score: " + humanScore +" Computer Score: " + computerScore);           
+                currentResultDiv.textContent = "You Lose! Paper beats Rock.";
+                scoreDiv.textContent = "Your Score: " + humanScore +" Computer Score: " + computerScore;           
             }
             else if(humanChoice === "paper" && computerChoice === "rock"){
                 humanScore++;
-                alert("You Win! Paper beats Rock.");
-                alert("Your Score: " + humanScore +" Computer Score: " + computerScore);  
+                currentResultDiv.textContent = "You Win! Paper beats Rock.";
+                scoreDiv.textContent = "Your Score: " + humanScore +" Computer Score: " + computerScore;  
             }
             else if(humanChoice === "scissors" && computerChoice === "paper"){
                 humanScore++;
-                alert("You Win! Scissors beats Paper.");
-                alert("Your Score: " + humanScore +" Computer Score: " + computerScore);  
+                currentResultDiv.textContent = "You Win! Scissors beats Paper.";
+                scoreDiv.textContent = "Your Score: " + humanScore +" Computer Score: " + computerScore;  
             }
             else if(humanChoice === "paper" && computerChoice === "scissors"){
                 computerScore++;
-                alert("You Lose! Scissors beats paper");
-                alert("Your Score: " + humanScore +" Computer Score: " + computerScore);  
+                currentResultDiv.textContent = "You Lose! Scissors beats paper.";
+                scoreDiv.textContent = "Your Score: " + humanScore +" Computer Score: " + computerScore;  
             }
             else if(humanChoice === "rock" && computerChoice === "scissors"){
                 humanScore++;
-                alert("You Win! Rock beats Scissors");
-                alert("Your Score: " + humanScore +" Computer Score: " + computerScore);  
+                currentResultDiv.textContent = "You Win! Rock beats Scissors.";
+                scoreDiv.textContent = "Your Score: " + humanScore +" Computer Score: " + computerScore;  
             }
             else{
                 computerScore++;
-                alert("You Lose! Rock beats Scissors.");
-                alert("Your Score: " + humanScore +" Computer Score: " + computerScore);  
+                currentResultDiv.textContent = "You Lose! Rock beats Scissors.";
+                scoreDiv.textContent = "Your Score: " + humanScore +" Computer Score: " + computerScore;  
             }
         }
-        playRound(humanChoice,computerChoice);
-        
- 
-      
-    if(humanScore > computerScore){
-        alert("Your Score: " + humanScore +" Computer Score: " + computerScore);  
-        alert("You Win!");
-
-    } 
-    else{
-        alert("Your Score: " + humanScore +" Computer Score: " + computerScore);  
-        alert("You Lose!");
-    }
-
+        function gameOver(){
+            if(humanScore === 5 || computerScore === 5){
+                if(humanScore > computerScore){
+                    finalResult.textContent = "You are the Winner!";
+                }
+                else{
+                    finalResult.textContent = "Better Luck Next Time!!";
+                }
+            }
+            return;
+        }
 
 }
+
 playGame();
 
